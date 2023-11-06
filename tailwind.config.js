6307e0ce08b87c1e14998bf72f082769a28b32c8
Aleksander Kuźma <aleksander.a.kuzma@gmail.com>
2023-11-06 23:10:24 +0000

    feat: add basic buttons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./public/**/*.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./public/**/*.html"],
+    theme: {
+        colors: {
+            background: 'hsl(var(--color-background) / <alpha-value>)',
+            foreground: 'hsl(var(--color-foreground) / <alpha-value>)',
+            muted: 'hsl(var(--color-muted) / <alpha-value>)',
+            "muted-foreground": 'hsl(var(--color-muted-foreground) / <alpha-value>)',
+            card: 'hsl(var(--color-card) / <alpha-value>)',
+            "card-foreground": 'hsl(var(--color-card-foreground) / <alpha-value>)',
+            popover: 'hsl(var(--color-popover) / <alpha-value>)',
+            "popover-foreground": 'hsl(var(--color-popover-foreground) / <alpha-value>)',
+            border: 'hsl(var(--color-border) / <alpha-value>)',
+            input: 'hsl(var(--color-input) / <alpha-value>)',
+            primary: 'hsl(var(--color-primary) / <alpha-value>)',
+            "primary-foreground": 'hsl(var(--color-primary-foreground) / <alpha-value>)',
+            secondary: 'hsl(var(--color-secondary) / <alpha-value>)',
+            "secondary-foreground": 'hsl(var(--color-secondary-foreground) / <alpha-value>)',
+            accent: 'hsl(var(--color-accent) / <alpha-value>)',
+            "accent-foreground": 'hsl(var(--color-accent-foreground) / <alpha-value>)',
+            destructive: 'hsl(var(--color-destructive) / <alpha-value>)',
+            "destructive-foreground": 'hsl(var(--color-destructive-foreground) / <alpha-value>)',
+            ring: 'hsl(var(--color-ring) / <alpha-value>)',
+            radius: 'hsl(var(--color-radius) / <alpha-value>)',
+        },
+        extend: {},
+    },
+    plugins: [],
 }
-
